1d7bd2e893b944c039dbc1d9f1e8aa18dc8bbcd0
Dima Bozhkov <dmitrijbozhkov@gmail.com>
2018-12-11 20:27:53 +0100

    Autogenerated files

---
+++ app/src/test/java/gas_station_console_mobile/nure/org/gasstationconsolemobile/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package gas_station_console_mobile.nure.org.gasstationconsolemobile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
